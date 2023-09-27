4e5473498650a8484009998a628b5839022cff96
sanitbhatkar <sanitcoc@gmail.com>
2023-09-27 18:19:40 +0530

    1.dill library for saving pickle file is installed. Requirements.txt is updated 2.utils.py has save object function which can now be used by all the files 3.data_ransformation code is written. It saves a pickle file 4.Test_runner file is created for checking if the code works fine or not. It is more like a wrapper.

---
+++ src/components/test_runner.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+from src.logger import logging
+from src.exception import CustomException
+
+from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
+
+if __name__=="__main__":
+
+    data_injestion=DataIngestion()
+    train_data_path,test_data_path=data_injestion.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
